a9fe899409d63aa95cbdb54de3d6a892f547a7ac
Vedant Avinash Patil <vedapati@cisco.com>
2026-07-27 10:23:07 +0530

    test: enforce OneAPI authentication contract

---
+++ tests/test_authentication_contract.py
@@ -0,0 +1,46 @@
+# Copyright (c) 2026 Splunk Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from src.asset import Asset
+
+
+def test_asset_uses_oneapi_authentication_contract() -> None:
+    configuration = Asset.to_json_schema()
+
+    assert {
+        "vanity_domain",
+        "client_id",
+        "client_secret",
+        "cloud",
+    } <= configuration.keys()
+    assert {
+        "base_url",
+        "api_key",
+        "username",
+        "password",
+        "sandbox_base_url",
+        "sandbox_api_token",
+    }.isdisjoint(configuration)
+
+    assert configuration["vanity_domain"]["required"] is True
+    assert configuration["client_id"]["required"] is True
+    assert configuration["client_secret"]["required"] is True
+    assert configuration["client_secret"]["data_type"] == "password"
+
+
+def test_sandbox_credentials_are_separate_and_optional() -> None:
+    configuration = Asset.to_json_schema()
+
+    assert configuration["sandbox_token"]["required"] is False
+    assert configuration["sandbox_token"]["data_type"] == "password"
+    assert configuration["sandbox_cloud"]["required"] is False
